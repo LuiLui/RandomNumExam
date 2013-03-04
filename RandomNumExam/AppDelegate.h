5f1589648559903f0274589c4c0063d056cd2803
LuiLui <tororo915@dualverse.com>
2013-03-04 10:21:26 +0900

    Initial Commit

---
+++ RandomNumExam/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RandomNumExam
+//
+//  Created by 이종운 on 13. 3. 4..
+//  Copyright (c) 2013년 Dualverse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RandomNumExam/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RandomNumExam
+//
+//  Created by 이종운 on 13. 3. 4..
+//  Copyright (c) 2013년 Dualverse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
